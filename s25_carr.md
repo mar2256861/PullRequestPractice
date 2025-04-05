21c45ce16f488be4e421c055448aa5d25ecb58c1
gitmacarr <mar2256861@maricopa.edu>
2025-04-05 12:28:38 -0700

    Added a quote from "Omerta" by Lamb of God in both s25_carr.md and s25_carr.py.

---
+++ s25_carr.md
@@ -0,0 +1,6 @@
+# Song Quote
+
+> "Whoever appeals to the law against his fellow man is either a fool or a coward. Whoever cannot take care of himself without that law is both. For a wounded man shall say to his assailant, 'If I die, you are forgiven. If I live, I will kill you.' Such is the rule of honor."
+
+\- Lamb of God
+ ("Omerta" (2004), Ashes of the Wake)
---
+++ s25_carr.py
@@ -0,0 +1,14 @@
+"""
+Mark Carr
+Module 08 Programming Project
+
+This is a quote in a forked repository.
+"""
+
+print("Whoever appeals to the law against his fellow man is either a fool or a coward.")
+print("Whoever cannot take care of himself without that law is both.")
+print("For a wounded man shall say to his assailant,")
+print(
+    "'If I die, you are forgiven. If I live, I will kill you.' Such is the rule of honor."
+)
+print("— Lamb of God, 'Omerta' (2004), Ashes of the Wake")
